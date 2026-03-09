24f762eccf2ee417a8043fa517be55a344332a35
sonu7071 <sky7070db@gmail.com>
2026-03-09 03:06:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -14,9 +14,8 @@ document.getElementById("pinmsg").innerText="Invalid Pincode"
 
 }
 
-function buyNow(){
 
-document.body.classList.add("loading")
+function buyNow(){
 
 setTimeout(()=>{
 
@@ -25,35 +24,3 @@ location.href="address.html"
 },1000)
 
 }
-
-function confirmOrder(){
-
-setTimeout(()=>{
-
-location.href="success.html"
-
-},2000)
-
-}
-
-function randomOrder(){
-
-let a=Math.floor(100+Math.random()*900)
-let b=Math.random().toString(36).substring(2,4).toUpperCase()
-let c=Math.floor(100000+Math.random()*900000)
-
-return a+b+c
-
-}
-
-document.addEventListener("DOMContentLoaded",()=>{
-
-let o=document.getElementById("orderid")
-
-if(o){
-
-o.innerText="Order ID: "+randomOrder()
-
-}
-
-})
