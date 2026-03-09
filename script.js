98f19d3aa1e513fc6ebfe0dab2d8680a2e92e01e
sonu7071 <sky7070db@gmail.com>
2026-03-09 02:38:27 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,57 @@
+function checkPin(){
+
+let pin=document.getElementById("pin").value
+
+if(pin.length==6){
+
+document.getElementById("pinmsg")
+.innerText="Delivery Available"
+
+}else{
+
+document.getElementById("pinmsg")
+.innerText="Invalid Pincode"
+
+}
+
+}
+
+function buyNow(){
+
+document.body.classList.add("loading")
+
+setTimeout(()=>{
+location.href="address.html"
+},1000)
+
+}
+
+function confirmOrder(){
+
+setTimeout(()=>{
+location.href="success.html"
+},2000)
+
+}
+
+function randomOrder(){
+
+let a=Math.floor(100+Math.random()*900)
+let b=Math.random().toString(36).substring(2,4).toUpperCase()
+let c=Math.floor(100000+Math.random()*900000)
+
+return a+b+c
+
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+let o=document.getElementById("orderid")
+
+if(o){
+
+o.innerText="Order ID: "+randomOrder()
+
+}
+
+})
